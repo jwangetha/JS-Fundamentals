72e47d9d292a813a0495c3e6812c27c83554d220
James Njoroge <jwangetha@gmail.com>
2025-09-10 17:03:02 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,1 +1,15 @@
+const args = process.argv.slice(2);
+if (args.length === 0 || isNaN(parseInt(args[0], 10))) {
+  console.log("Missing number of occurrences");
+} else {
+  const count = parseInt(args[0], 10);
+  let i = 0;
+  let output = "";
+  while (i < count) {
+    output += "C is fun\n";
+    i++;
+  }
+  console.log(output.trim());
+}
+
 
